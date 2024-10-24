e891adc7247980aa9e92f06d60acc189eb89958f
Leonardo <lsr_31@hotmail.com>
2024-10-24 09:09:21 -0400

    fixing bugs

--- models/transformed/currency_volatility_analysis.sql
+++ models/transformed/currency_volatility_analysis.sql
@@ -1,11 +1,13 @@
 WITH volatility_analysis AS (
     SELECT
-        DATE_TRUNC('month', date) AS month, -- Agrupa por mes
-        EXTRACT(YEAR FROM date) AS year, -- Extrae el año
+        DATE_TRUNC('month', date) AS month, 
+        EXTRACT(YEAR FROM date) AS year, 
         country,
         currency,
         STDDEV(exchange_rate_bs) AS stddev_bs,
-        STDDEV(exchange_rate_me) AS stddev_me
+        STDDEV(exchange_rate_me) AS stddev_me,
+        AVG(exchange_rate_bs) AS avg_exchange_rate_bs, 
+        AVG(exchange_rate_me) AS avg_exchange_rate_me 
     FROM
         {{ ref('normalized_exchange_rates') }}
     GROUP BY
